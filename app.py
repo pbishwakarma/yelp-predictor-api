1cfcaa29cca80113d05f27a7b85fba08e4aefe7e
Prajal Bishwakarma <pbishwakarma@email.wm.edu>
2017-02-17 11:29:06 -0500

    add port

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ def main():
 	return re
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=os.getenv("PORT"), debug=True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host="0.0.0.0", port=port, debug=True)
 	#  
